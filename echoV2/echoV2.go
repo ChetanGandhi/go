8b33e8e36b2157f70e3f3465e00f48854abc6bbb
Chetan Gandhi <projectchetan07@gmail.com>
2016-03-02 01:47:12 +0530

    create echo v2 which prints command line arguments using range method.

---
+++ echoV2/echoV2.go
@@ -0,0 +1,18 @@
+// Print command line arguments using simple for loop and range.
+package main
+
+import "fmt"
+import "os"
+
+func main() {
+	statment, seperator := "", ""
+
+    // range returns two values, index and value of the element at that index.
+    // We do not need index hence specifying "_" the "blank identifier", as go do not allow to have unused variables.
+	for _, argument := range os.Args[1:] {
+		statment += seperator + argument
+		seperator = " "
+	}
+
+	fmt.Println(statment)
+}
