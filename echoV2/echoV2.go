5c863d9a075dc21a76856f301fce39c635cc7a8e
Chetan Gandhi <projectchetan07@gmail.com>
2016-03-02 02:39:03 +0530

    modified logic to print executable name also.

--- echoV2/echoV2.go
+++ echoV2/echoV2.go
@@ -10,7 +10,7 @@ func main() {
 
 	// range returns two values, index and value of the element at that index.
 	// We do not need index hence specifying "_" the "blank identifier", as go do not allow to have unused variables.
-	for _, argument := range os.Args[1:] {
+	for _, argument := range os.Args[0:len(os.Args)] {
 		statment += seperator + argument
 		seperator = " "
 	}
